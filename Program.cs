ca218e73ea9a94efb2c2e14e5486b506cb1b0fc5
Hassaan Anas <hassaananas@usf.edu>
2021-01-20 16:50:05 -0500

    Version 4 - Changes made

--- Program.cs
+++ Program.cs
@@ -6,8 +6,11 @@ namespace HelloWorld8
     {
         static void Main(string[] args)
         {
-          
+            // Pause to display hello world!
+            Console.WriteLine("Press any key to continue...");
             Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+
         }
     }
 }
